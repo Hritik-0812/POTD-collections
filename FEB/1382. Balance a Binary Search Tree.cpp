3618e37bfbef9bf96de0d34dd74176da1a2fa755
Hritik-0812 <2023493345.Hritik@ug.sharda.ac.in>
2026-02-09 21:45:02 +0530

    09 Solution

---
+++ FEB/1382. Balance a Binary Search Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ /************************************************************ C++ ************************************************************/
+//Approach : store the nodes in a vector using inorder traversal(sorted fashion ) and then build the tree using the vector(using concept of merge sort)
+//T.C : O(2n)
+//S.C : O(2n)
+class Solution {
+public:
+    void inorder(vector<TreeNode*>&nodes,TreeNode* root){
+        if(root==NULL)return ;
+        inorder(nodes,root->left);
+        nodes.push_back(root);
+        inorder(nodes,root->right);
+    }
+    TreeNode* build(vector<TreeNode*>&nodes,int l,int r){
+        if(l>r)return NULL;
+        int mid=(l+r)/2;
+
+        TreeNode *root=nodes[mid];
+        int n=nodes.size();
+        root->left=build(nodes,l,mid-1);
+        root->right=build(nodes,mid+1,r);
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<TreeNode*>nodes;
+        inorder(nodes,root);
+        int n=nodes.size();
+        return build(nodes,0,n-1);
+    }
+};
